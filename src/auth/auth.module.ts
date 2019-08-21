83333c8c3f4af37e3d811a4e2a66e22ea4534c74
Jamie Strange <jamie.strange@unosquare.com>
2019-08-21 14:31:47 +0100

    removing google auth files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { GoogleStrategy } from './google.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
@@ -7,6 +6,6 @@ import { AzureStrategy } from './azure.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, JwtStrategy, AzureStrategy],
+  providers: [AuthService, JwtStrategy, AzureStrategy],
 })
 export class AuthModule {}
