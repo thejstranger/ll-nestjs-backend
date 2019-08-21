5a6d098893e2e9e9e16172ff8e38783fc3664db8
Jamie Strange <jamie.strange@unosquare.com>
2019-08-21 14:30:34 +0100

    removing google auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,18 +3,6 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  @Get('google')
-  @UseGuards(AuthGuard('google'))
-  public async googleLogin() {}
-
-  @Get('google/callback')
-  @UseGuards(AuthGuard('google'))
-  public async googleLoginCallback(@Req() req, @Res() res) {
-    const jwt: string = req.user.jwt;
-    if (jwt) res.redirect('http://localhost:4200/login/success/' + jwt);
-    else res.redirect('http://localhost:4200/login/failure');
-  }
-
   @Get('protected')
   @UseGuards(AuthGuard('jwt'))
   protectedResource() {
