7c9063a913f8254230f67a074db604e05a0258c5
Jamie Strange <jamie.strange@unosquare.com>
2019-08-22 14:54:52 +0100

    AccessToken being returned from Azure AD

--- src/auth/azure.strategy.ts
+++ src/auth/azure.strategy.ts
@@ -10,49 +10,48 @@ var users = {};
 @Injectable()
 export class AzureStrategy extends PassportStrategy(OIDCStrategy, 'azure') {
   constructor(private readonly authService: AuthService) {
-    super({
-      identityMetadata:
-        'https://login.microsoftonline.com/eedd1340-df1a-4db2-8a03-b4cfb1fa3e9d/v2.0/.well-known/openid-configuration',
-      clientID: '2a075ba4-9d57-4219-b99f-e677392bc809',
-      clientSecret: 'HrrDw:4Y-OGqG2LMRkr5lfpuA/8pnyd:',
-      allowHttpForRedirectUrl: true,
-      redirectUrl: 'http://localhost:3000/auth/azure/callback',
-      passReqToCallback: false,
-      responseType: 'code id_token',
-      responseMode: 'form_post',
-      scope: ['profile', 'offline_access', 'user.read', 'calendars.read'],
-    });
-  }
+    super(
+      {
+        identityMetadata:
+          'https://login.microsoftonline.com/eedd1340-df1a-4db2-8a03-b4cfb1fa3e9d/v2.0/.well-known/openid-configuration',
+        clientID: '2a075ba4-9d57-4219-b99f-e677392bc809',
+        clientSecret: 'HrrDw:4Y-OGqG2LMRkr5lfpuA/8pnyd:',
+        allowHttpForRedirectUrl: true,
+        redirectUrl: 'http://localhost:3000/auth/azure/callback',
+        passReqToCallback: false,
+        responseType: 'code id_token',
+        responseMode: 'form_post',
+        scope: ['profile', 'offline_access', 'user.read', 'calendars.read'],
+      },
+      function(iss, sub, profile, access_token, refresh_token, done) {
+        console.log(profile);
+        console.log(profile.oid);
+        if (!profile.oid) {
+          return done(new Error('No Oid Found'), null);
+        }
+        const jwt: string = this.authService.validateOIDCLogin(
+          profile.id,
+          Provider.AZURE,
+        );
 
-  async validate(profile, done) {
-    try {
-      console.log(profile);
-      console.log(profile.oid);
-      if (!profile.oid) {
-        return done(new Error('No Oid Found'), null);
-      }
-      const jwt: string = await this.authService.validateOIDCLogin(
-        profile.id,
-        Provider.AZURE,
-      );
+        const user = {
+          jwt,
+        };
 
-      const user = {
-        jwt,
-      };
+        //console.log(user);
+        //   console.log(profile._json.aio);
+        console.log('access token = ' + access_token);
+        console.log(sub);
 
-      console.log(user);
-      console.log(profile._json.aio);
+        const client = getAuthenticatedClient(access_token);
+        //const te = await client.api('/me').get();
+        console.log();
 
-      const client = getAuthenticatedClient(access_token);
-      const te = await client.api('/me').get();
-      console.log(te);
+        console.log(profile);
 
-      console.log(profile);
-
-      return done(null, user);
-    } catch (err) {
-      return done(err, false);
-    }
+        return done(null, user);
+      },
+    );
   }
 }
 
